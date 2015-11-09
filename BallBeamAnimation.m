f4644194a72a7efbb35e9cc0714d42989c2a5836
Michael <mikeandike1994@gmail.com>
2015-11-08 20:44:38 -0500

    Removed erroneous comments

--- BallBeamAnimation.m
+++ BallBeamAnimation.m
@@ -15,8 +15,7 @@
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
-% Record keeping code for how long it takes to simulate (deprecated)
+% initial setup
 clear global
 clearvars
 
